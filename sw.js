740177570c261cfa7172d772310ff7219eeec931
ryanzhang66 <2501307729@qq.com>
2024-07-19 02:21:13 +0800

    Site updated: 2024-07-19 02:21:13

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1721326621452";
+const VERSION = "1721326869210";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
