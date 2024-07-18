b720335f1cda74bdc22e4d56124470fcbe79b33b
ryanzhang66 <2501307729@qq.com>
2024-07-19 02:40:54 +0800

    Site updated: 2024-07-19 02:40:54

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1721327924178";
+const VERSION = "1721328053684";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
