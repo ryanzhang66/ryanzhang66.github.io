1a430d3a9d013f06be83c23e5682566b4a5b056f
ryanzhang66 <ryanzhang66@users.noreply.github.com>
2026-04-08 21:07:46 +0800

    Site updated: 2026-04-08 21:07:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1775653097440";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1775653664201";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
